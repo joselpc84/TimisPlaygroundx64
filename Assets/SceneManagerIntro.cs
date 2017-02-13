c40aa532bc3ff8db00f3c1f0d7d02ef38e4e6f28
joselpc84 <joselpc84@gmail.com>
2017-02-13 18:29:25 -0400

    Avance Intro

--- Assets/SceneManagerIntro.cs
+++ Assets/SceneManagerIntro.cs
@@ -5,16 +5,44 @@ using UnityEngine;
 public class SceneManagerIntro : MonoBehaviour {
 
 	public Camera mainCamera;
+	public Transform intro;
+	public Transform memoria;
+	public Transform flechaDer;
+	public Transform flechaIzq;
 
 
 	// Use this for initialization
 	void Start () {
+		memoria.gameObject.SetActive(false);
+		flechaDer.gameObject.SetActive(false);
+		flechaIzq.gameObject.SetActive(false);
 		StartCoroutine(InitTween());
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if(intro.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime > 0.75f)
+		{
+			memoria.gameObject.SetActive(true);
+			flechaDer.gameObject.SetActive(true);
+
+		}
+
+		//Debug.Log(intro.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Intro"));
+		//Debug.Log(intro.GetComponent<Animator>().);
+		/*
+		if(intro.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Intro") && GlobalVar.introAnimFinished == false)
+		{
+			intro.GetComponent<Animator>().SetBool("IntroAnimFinished", true);
+			GlobalVar.introAnimFinished = true;
+		}
+		if(GlobalVar.introAnimFinished == true)
+		{
+			memoria.gameObject.SetActive(true);
+		}
+		*/
 	}
 
 	IEnumerator InitTween(){
